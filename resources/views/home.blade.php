ed0a17a29378f646ea10164cdc299f3b42fc7717
pcorsario <patriciovacaescbar@gmail.com>
2022-09-04 11:14:05 -0500

    Added ProductoController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,7 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-CAMBIOS REALIZADOS DESDE VSCODE
-                    {{ __('You are logged in!') }}
+                    
                 </div>
             </div>
         </div>
